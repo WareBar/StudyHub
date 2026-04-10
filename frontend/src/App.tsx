2c2241ad3fb4fe5d7af2280cc402b08192aa20a0
Abdul Barry A. Adam <barryadam615@gmail.com>
2026-04-10 19:04:53 +0800

    library: installed tanstack

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,17 +3,20 @@ import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { Toaster } from "@/components/ui/sonner"
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient()
 
 function App() {
-
-
   return (
-    <AuthProvider>
-      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-        <RouterProvider router={router}/>
-      </GoogleOAuthProvider>
-      <Toaster/>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+          <RouterProvider router={router}/>
+        </GoogleOAuthProvider>
+        <Toaster/>
+      </AuthProvider>
+    </QueryClientProvider>
   )
 }
 
