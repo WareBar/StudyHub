865e5b3f2a288b150bb80a6f7e2e6953609bcc5c
Abdul Barry A. Adam <barryadam615@gmail.com>
2026-04-24 19:41:33 +0800

    feat: added search user, profile model and moved stats to dedicated service and created simple serializer to avoid circular import

--- backend/User/serializers.py
+++ backend/User/serializers.py
@@ -1,16 +1,22 @@
-from User.models import User
+from User.models import User, Profile
 from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from study.serializers import SubjectSerializer
+from study.models import Subject
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
         extra_kwargs = {
-            "password":{"write_only":True}
+            "password": {"write_only": True}
         }
 
+class ProfileSerializer(ModelSerializer):
+    user = UserSerializer(many=False)
+    subjects_of_interest_detail = SubjectSerializer(source="subjects_of_interest", read_only=True, many=True)
+    subjects_of_interest = serializers.PrimaryKeyRelatedField(queryset=Subject.objects.all(), write_only=True)
 
-class SimpleUserSerializer(ModelSerializer):
     class Meta:
-        model = User
-        fields = ["id","username", "first_name","last_name","email", "avatar"]
+        model = Profile
+        fields = "__all__"
