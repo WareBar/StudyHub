7d226c3a1d59aff49d1f56208a7a6e5fa6e21897
Abdul Barry A. Adam <barryadam615@gmail.com>
2026-04-24 19:53:49 +0800

    feat: created scripts to generate types from backend to frontend
    created scripts  to generate types from backend to frontend and stored them into models.ts
    
    also installed tabler/icons

---
+++ frontend/src/types/models.ts
@@ -0,0 +1,10 @@
+import type { components } from "./api";
+
+export type UserProps = components["schemas"]["User"]
+export type StudyGroupProps = components["schemas"]["StudyGroup"]
+export type SubjectProps = components["schemas"]["Subject"]
+export type AttendanceProps = components["schemas"]["Attendance"]
+export type SessionProps = components["schemas"]["Session"]
+export type ChatProps = components["schemas"]["Chat"]
+export type MembershipProps = components["schemas"]["MemberShip"]
+// export type sssssssssss
